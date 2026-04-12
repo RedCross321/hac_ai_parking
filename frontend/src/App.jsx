10f0d49dc94fcdd8aa17f6aab12423df7b40814c
Seyshiro <kostay_noskov_97@mail.ru>
2026-04-12 08:31:40 +0500

    desktop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage'
